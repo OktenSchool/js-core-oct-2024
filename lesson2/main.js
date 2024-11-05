cfdaac895941f4c0c965318aea7bc7a3a3399bac
GrayHead <ss.zhuravlov@gmail.com>
2024-11-05 19:51:21 +0200

    lesson 2

---
+++ lesson2/main.js
@@ -0,0 +1,77 @@
+// let object1 = {
+//     name: 'Sergii',
+//     surname: 'Zhuravlov',
+//     age: 36,
+//     status: true
+// }
+//
+// let object2 = {
+//     name: 'Vasya',
+//     surname: 'Pukin',
+//     age: 31,
+//     status: false
+// }
+//
+// console.log(object1);
+// console.log(object2);
+// console.log(object1.name);
+// console.log(object2.name);
+
+// let user1 = {
+//     id: 'RrnK9ysCtei',
+//     name: 'Vaysa',
+//     surname: 'Gubkin',
+//     age: 31,
+//     status: true,
+//     wife: {
+//         name: 'Anna',
+//         age: 29,
+//         height: 175,
+//         family: [
+//             {type: 'mother', name: 'Natalia', age: 55, status: false},
+//             {type: 'father', name: 'Ihor', age: 59, status: true},
+//             {type: 'brother', name: 'Max', age: 19, status: true},
+//             {type: 'sister', name: 'Olya', age: 19, status: true},
+//             {type: 'sister', name: 'Hanna', age: 19, status: false},
+//         ],
+//         skills: [{title: 'htlm', exp: 14}, {title: 'js', exp: 12}]
+//     }
+//
+// }
+// console.log(user1);
+// console.log(user1.name);
+//
+// console.log(user1.wife.name);
+//
+//
+//
+// console.log(user1.wife.skills[0]);
+// console.log(user1.wife.skills[1]);
+
+//                                      0     1    2         3
+// let arr = [123, 234, 456, {name: 'kokos'}];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[3]);
+// console.log(arr[3].name);
+
+
+let a = 100;
+let b = a;
+b = 200;
+
+console.log(a, b);
+
+let objA = {value: 100}
+let objB = objA;
+
+objB.value = 200;
+
+console.log(objA.value, objB.value);
+
+
+
+
+
+
+
